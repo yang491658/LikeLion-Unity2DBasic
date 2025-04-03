b7fbf48d98f9585261325216c2b955244a6fbd14
yang491658 <park49164357@gmail.com>
2025-04-03 11:29:01 +0900

    250403 1128

--- ShadowDash/Assets/Scripts/Player.cs
+++ ShadowDash/Assets/Scripts/Player.cs
@@ -10,15 +10,20 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed; // 속도
     [SerializeField] private float jump; // 점프력
 
-    [SerializeField] private bool move; // 이동 여부
-
     private int dircetion = 1; // 방향
     private bool isRight = true;
 
-    [Header("Collision Info")]
-    [SerializeField] private float distance;
+    [Header("충돌 정보")]
+    [SerializeField] private float distance; // 충돌 감지 거리
     [SerializeField] private LayerMask ground;
-    private bool isGround;
+    private bool isGround; // 충돌 여부
+
+    [Header("대쉬 정보")]
+    [SerializeField] private float dashSpeed; // 대쉬 속도
+    [SerializeField] private float dashDuration; // 대쉬 지속 시간
+    private float dashTimer; // 대쉬 발동 타이머
+    [SerializeField] private float dashCooldown; // 대쉬 쿨타임
+    private float dashCooldownTimer; // 대쉬 쿨타임 타이머
 
     private void Start()
     {
@@ -32,10 +37,15 @@ public class Player : MonoBehaviour
         Move(); // 플레이어 이동
         FlipControl(); // 플레이어 이동 중 방향 전환
         CollisionCheck(); // 충돌 체크
+
+        // 대쉬 타이머
+        dashTimer -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+
         AnimationControl(); // 플레이어 이동 모션
     }
 
-    private void InputKey()
+    private void InputKey() // 키 입력
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
@@ -44,21 +54,43 @@ public class Player : MonoBehaviour
 
         // 플레이어 점프
         if (Input.GetKeyDown(KeyCode.Space)) Jump();
+
+        // 플레이어 대쉬
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            CanDash();
+        }
     }
 
-    private void Move()
+    private void CanDash() // 대쉬 가능
     {
-        rb.linearVelocity = new Vector2(xInput * speed, rb.linearVelocityY);
+        if (dashCooldownTimer < 0)
+        {
+            dashTimer = dashDuration;
+            dashCooldownTimer = dashCooldown;
+        }
     }
 
-    private void Flip()
+    private void Move() // 이동 및 대쉬
+    {
+        if (dashTimer > 0)
+        {
+            rb.linearVelocity = new Vector2(xInput * dashSpeed, rb.linearVelocityY);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(xInput * speed, rb.linearVelocityY);
+        }
+    }
+
+    private void Flip() // 방향 전환
     {
         dircetion = dircetion * -1;
         isRight = !isRight;
         transform.Rotate(0, 180, 0);
     }
 
-    private void FlipControl()
+    private void FlipControl() // 방향 전환 컨트롤
     {
         if (rb.linearVelocityX > 0 && !isRight)
         {
@@ -70,24 +102,34 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void Jump() // 점프
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
+        if (isGround) rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmos() // 기즈모 그리기
     {
-        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - distance));
+        Gizmos.DrawLine(transform.position,
+            new Vector3(transform.position.x,
+            transform.position.y - distance));
     }
 
-    private void CollisionCheck()
+    private void CollisionCheck() // 충돌 감지
     {
         isGround = Physics2D.Raycast(transform.position, Vector2.down, distance, ground);
     }
 
-    private void AnimationControl()
+    private void AnimationControl() // 애니메이션 컨트롤
     {
-        move = rb.linearVelocity.x != 0;
+        // 이동 모션
+        bool move = rb.linearVelocity.x != 0;
         ani.SetBool("Move", move);
+
+        // 점프 및 낙하 모션
+        ani.SetBool("IsGround", isGround);
+        ani.SetFloat("ySpeed", rb.linearVelocityY);
+
+        // 대쉬 모션
+        ani.SetBool("Dash", dashTimer > 0);
     }
 }
